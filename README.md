8240af3851234d2708648336725637dc4b6f783d
Robert Lee <rlee32@gatech.edu>
2019-01-25 01:59:38 -0500

    enhance clean script, small readme todo list

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ NACA0012 OpenFOAM results are compared to experimental data.
 1. Set the desired angle of attack: variable "aoa" (degrees) in mesh/parameters.geo.
 2. ./run.sh (see script for sub-steps)
 3. Open "view.foam" in ParaView and/or view force coefficients in ./case/postProcessing/.
+
+TODO:
+1. Implement batch run, post-processing, and plotting script.
--- clean.py
+++ clean.py
@@ -2,16 +2,23 @@
 
 import os
 
+def delete_if_exists(path):
+    if os.path.exists(path):
+        os.system("rm -r " + path)
+
 folders = [x for x in os.listdir("case/") if os.path.isdir("case/" + x)]
 for f in folders:
+    if "processor" in f: # in the case of parallel runs.
+        delete_if_exists("case/" + f)
+        continue
     dotsep = f.split(".")
     isfloat = len(dotsep) > 1 and all([x.isdigit() for x in dotsep])
     esep = f.split("e-")
     issci = len(esep) > 1 and all([x.isdigit() for x in esep])
     isnum = f.isdigit() or isfloat or issci
     if isnum and float(f) != 0:
-        os.system("rm -r case/" + f)
-os.system("rm main.msh")
-os.system("rm -r case/constant/polyMesh")
-os.system("rm -r case/postProcessing")
+        delete_if_exists("case/" + f)
 
+delete_if_exists("main.msh")
+delete_if_exists("case/constant/polyMesh")
+delete_if_exists("case/postProcessing")
